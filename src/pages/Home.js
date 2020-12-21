9e1859ad3dd763c679498f705167de612a219b20
Gaspar Dolcemascolo <gaspardolcemascolo@gmail.com>
2020-12-21 11:08:06 -0300

    home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,52 @@
 const Home = () => {
 	const view = `
-		
+		<section class="appContent-wraper">
+			<div class="pin">
+				<img src="https://i.pinimg.com/236x/65/14/19/65141958d83caacae8d698b2cce2631c.jpg">
+				<!-- <div class="save-pin">
+					<p>Save</p>
+				</div>
+				<div class="pin-options">
+					<i class="pin-ico icon-search"></i>
+				</div> -->
+			</div>
+			<div class="pin">
+				<img src="https://i.pinimg.com/236x/c8/f0/77/c8f077f4ea9e316a58ae9dabe0468888.jpg">
+				<!-- <div class="save-pin">
+					<p>Save</p>
+				</div>
+				<div class="pin-options">
+					<i class="pin-ico icon-search"></i>
+				</div> -->
+			</div>
+			<div class="pin">
+				<img src="https://i.pinimg.com/236x/8d/3e/3e/8d3e3e01f4fabfc6d17902846bdc1f35.jpg">
+				<!-- <div class="save-pin">
+					<p>Save</p>
+				</div>
+				<div class="pin-options">
+					<i class="pin-ico icon-search"></i>
+				</div> -->
+			</div>
+			<div class="pin">
+				<img src="https://i.pinimg.com/236x/8d/3e/3e/8d3e3e01f4fabfc6d17902846bdc1f35.jpg">
+				<!-- <div class="save-pin">
+					<p>Save</p>
+				</div>
+				<div class="pin-options">
+					<i class="pin-ico icon-search"></i>
+				</div> -->
+			</div>
+			<div class="pin">
+				<img src="https://i.pinimg.com/236x/e7/cd/d0/e7cdd06b08579dc1e8764ba5523c16fa.jpg">
+				<!-- <div class="save-pin">
+					<p>Save</p>
+				</div>
+				<div class="pin-options">
+					<i class="pin-ico icon-search"></i>
+				</div> -->
+			</div>
+		</section>
 	`
 	return view
 }
